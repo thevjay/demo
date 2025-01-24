165b0cba0fd9ea35cc42e17e25eafc0a355a6ab6
Vijay <rebalvijay930@gmail.com>
2025-01-25 00:06:03 +0530

    first commit

---
+++ bend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use("/",(req,res)=>{
+    res.send("Server is running")
+})
+
+app.listen(8000,()=>{
+    console.log(`Server started on PORT 8000`)
+})
